7222dc3409ae2ad9a572eb8adeade79b92904e2f
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2023-10-17 23:04:19 +0900

    feat: client 証明書付きコード作成

---
+++ test05/test03.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+)
+
+func main() {
+	cert, err := os.ReadFile("ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(cert)
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+	resp, err := client.Get("https://localhost:18443")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
+}
