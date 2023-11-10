7e018fba8f9f0d6a82f35b0a938e09047b5de11e
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2023-11-10 22:35:43 +0900

    feat: リバースプロキシ レスポンスの書き換え

---
+++ test07/test03.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"strconv"
+)
+
+func main() {
+	director := func(request *http.Request) {
+		request.URL.Scheme = "http"
+		request.URL.Host = ":9001"
+	}
+	modifier := func(res *http.Response) error {
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return fmt.Errorf("Reading body error: %w", err)
+		}
+		newBody := bytes.NewBuffer(body)
+		newBody.WriteString("via Proxy")
+		res.Body = io.NopCloser(newBody)
+		res.Header.Set("Content-Length", strconv.Itoa(newBody.Len()))
+		return nil
+	}
+
+	rp := &httputil.ReverseProxy{
+		Director:       director,
+		ModifyResponse: modifier,
+	}
+	server := http.Server{
+		Addr:    "127.0.0.1:9000",
+		Handler: rp,
+	}
+	log.Println("Start Listening at :9000")
+	log.Fatalln(server.ListenAndServe())
+}
